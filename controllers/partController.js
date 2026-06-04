370279c1bfdd65e253cfb6fd3bbfb9a4ef8cd0e8
Pedroca12345 <pepeplays8@gmail.com>
2026-06-04 19:30:56 -0300

    bug fixes rsrs e algumas mudanças de nomeclatura

--- controllers/partController.js
+++ controllers/partController.js
@@ -1,6 +1,6 @@
 const Part = require("../models/Part");
 
-const uploadPartController = async (req, res) => {
+const partController = async (req, res) => {
   const {
     partcode,
     quantity,
@@ -10,10 +10,15 @@ const uploadPartController = async (req, res) => {
 
   if (partcode && quantity && description && location) {
 
-    const existingPart = await Part.findOne({ partcode: partcode }).exec();
+    const existingPart = await Part.findOne(
+      {
+        partcode: partcode,
+        location: location
+      })
+      .exec();
 
-    if(existingPart) {
-      return res.status(400).json({ message: "Peça já cadastrada na base de dados" });
+    if (existingPart) {
+      return res.status(400).json({ message: partcode });
     }
 
     const part = {
@@ -25,10 +30,10 @@ const uploadPartController = async (req, res) => {
 
     await Part.create(part);
     return res.status(200).json({ message: "Peça cadastrada com sucesso" });
-    
+
   } else {
     return res.status(400).json({ message: "Dados inválidos" });
   }
 }
 
-module.exports = uploadPartController;
+module.exports = partController;
--- routes/partRoutes.js
+++ routes/partRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const uploadPartController = require("../controllers/uploadPartController");
+const partController = require("../controllers/partController");
 const authMiddleware = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-router.post("/part", authMiddleware, uploadPartController);
+router.post("/part", authMiddleware, partController);
 
 module.exports = router;
