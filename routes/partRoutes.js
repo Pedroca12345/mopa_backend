592298365679f2597e83893ffa0901c64eeb59fa
Pedroca12345 <pepeplays8@gmail.com>
2026-08-29 22:00:17 -0300

    adição das rotas de peças paginação e edição

--- routes/partRoutes.js
+++ routes/partRoutes.js
@@ -1,12 +1,14 @@
 const express = require("express");
 
-const { registerPartController, getPartController } = require("../controllers/partController");
+const { registerPartController, getPartController, getPartsController, updatePartsController } = require("../controllers/partController");
 
 const authMiddleware = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
 router.post("/part", authMiddleware, registerPartController);
-router.get("/part", authMiddleware, getPartController)
+router.get("/part", authMiddleware, getPartController);
+router.get("/parts", authMiddleware, getPartsController);
+router.put("/updatePart", authMiddleware, updatePartsController);
 
 module.exports = router;
